d8517cf1d53cddd25bf1428be2593640f4920e5e
Wilco Verhoef <a.w.verhoef@uu.nl>
2022-04-21 18:18:46 +0200

    Added Dashboard plugin

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 // The simplest LISK application
 
 const { Application } = require('lisk-sdk');
+const { DashboardPlugin } = require('@liskhq/lisk-framework-dashboard-plugin');
 
 const genesisBlock = require('./config/genesis-block.json');
 const config = require('./config/config.json');
 
 const app = Application.defaultApplication(genesisBlock, config);
 
+app.registerPlugin(DashboardPlugin);
+
 // Export function so CoSy can call it.
-module.exports = {
-    start() {
-        app.run();
-    }
-}
+exports.start = () => app.run();
 
 // Call start if file is ran by 'node index.js'
 if (require.main === module)
-    module.exports.start();
+    app.run();
