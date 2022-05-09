486f798f2609a3220711eefbd1fc444b827ad0d0
Art Schenkel <j.a.schenkel@students.uu.nl>
2022-05-09 15:26:07 +0200

    getpackageinfo update

--- modules/trustfacts/trustfacts_module.ts
+++ modules/trustfacts/trustfacts_module.ts
@@ -1,6 +1,7 @@
 import { BaseModule, codec } from 'lisk-sdk';
 import { TrustFactsSchema, trustFactsListSchema } from './trustfacts_schema'
 import { TrustFactsAddFactAsset } from './assets/addfact_asset'
+//import { codaJobListSchema } from '../coda/coda-schemas';
 
 export class TrustFactsModule extends BaseModule {
     id = 1234;
@@ -15,14 +16,24 @@ export class TrustFactsModule extends BaseModule {
         new TrustFactsAddFactAsset()
     ];
 
-
     actions = {
         // GET THE FACTS LIST
         getFacts: async () => {
             let trustFactsBuffer:any = await this._dataAccess.getChainState("trustfacts:facts");
             return codec.decode(trustFactsListSchema, trustFactsBuffer);
+        },
+
+        // GET ALL THE TRUSTFACTS FOR A SPECIFIC PACKAGE
+        getPackageInfo: async (packageName/*: Record<string, unknown>*/) => {
+            //get facts buffer for the given package
+            let trustFactsBuffer:any = await this._dataAccess.getChainState("trustfacts:" + packageName);
+            //decode facts buffer
+            let {facts} = codec.decode<{facts:{jobID:number,factData:string,gitSignature:string,keyURL:string}[]}>(trustFactsListSchema, trustFactsBuffer);
+            //if facts are available, return them
+            if(facts.length > 0) return facts;
+            else throw new Error("There are no trust-facts available for this package");
         }
-    };
+    }
 
     events = ['newFact'];
 
@@ -32,8 +43,5 @@ export class TrustFactsModule extends BaseModule {
             console.log('afterTransactionApply: fact:', fact);
             this._channel.publish('trustfacts:newFact', fact);
         }
-
     }
-
-
 }
