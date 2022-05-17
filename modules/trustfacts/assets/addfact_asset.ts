8a36354f15c9fe2b33d81d1c1bece756af295086
Geert Jansen <geert.jansen95@hotmail.com>
2022-05-17 15:21:56 +0200

    Added trustFact errorhandling, changed module ID, made fields required (#22)

--- modules/trustfacts/assets/addfact_asset.ts
+++ modules/trustfacts/assets/addfact_asset.ts
@@ -4,7 +4,7 @@ import { CodaJobList, codaJobListSchema } from '../../coda/coda-schemas';
 import { TrustFact, TrustFactList, TrustFactSchema, TrustFactListSchema } from '../trustfacts_schema';
 
 export class TrustFactsAddFactAsset extends BaseAsset {
-    static id = 12340;
+    static id = 32280;
     id = TrustFactsAddFactAsset.id;
     name = 'AddFacts';
     schema = TrustFactSchema;
@@ -13,7 +13,10 @@ export class TrustFactsAddFactAsset extends BaseAsset {
         // for now onlycheck if fields are not empty, TODO more logic
         
         //TODO: validate data and gpg key
-        if (asset.factData.trim() === "") throw new Error("Data cannot be empty");
+        if (asset.factData.trim() === "") throw new Error("FactData cannot be empty");
+        if (asset.gitSignature.trim() === "") throw new Error("GitSignature cannot be empty");
+        if (asset.keyURL.trim() === "") throw new Error("KeyUrl cannot be empty");
+        if (asset.jobID < 0) throw new Error("JobID can't be negative");
     }
 
     async apply({ asset, stateStore } : ApplyAssetContext<TrustFact>) {
@@ -21,7 +24,12 @@ export class TrustFactsAddFactAsset extends BaseAsset {
         // get the job
         const jobsBuffer = await stateStore.chain.get("coda:jobs") as Buffer;
         const { jobs } = codec.decode<CodaJobList>(codaJobListSchema, jobsBuffer);
-        const { package : pack } = jobs[asset.jobID];
+
+        // check if job id is correct 
+        // needs refactoring if we add a CoDa garbarge collector
+        if( asset.jobID > jobs.length - 1) throw new Error("JobID incorrect, job does not exist");
+
+        const { package : pack } = jobs[asset.jobID];   
 
         // get the facts for this package
         let facts : TrustFact[] = [];
@@ -30,7 +38,6 @@ export class TrustFactsAddFactAsset extends BaseAsset {
         if (trustFactsBuffer !== undefined) {
             facts = codec.decode<TrustFactList>(TrustFactListSchema, trustFactsBuffer).facts;
         }
-
         // add the new fact
         facts.push(asset);
 
