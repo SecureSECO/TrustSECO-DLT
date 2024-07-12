b2610df6c1cca75959067b865ff2486cdfd18984
martijnv <martijnvoordouw@gmail.com>
2024-07-12 16:27:55 +0200

    fixed a division by zero error This error war caused when a job had no trustfacts associated with it

--- modules/coda/coda-module.ts
+++ modules/coda/coda-module.ts
@@ -96,6 +96,13 @@ export class CodaModule extends BaseModule {
                     const { facts: allFacts } = codec.decode<TrustFactList>(TrustFactListSchema, trustFactsBuffer);
                     const facts = allFacts.filter(fact => fact.jobID == job.jobID);
 
+                    // if no facts were gathered nothing needs to be payed out, so we can continue
+                    // TODO: does this mean the requester doesn't get the tokens back?
+                    if (facts.length === 0)
+                    {
+                        continue;
+                    }
+
                     // calculate network capacity (total facts)
                     const jobsBuffer = await stateStore.chain.get("coda:jobs") as Buffer;
                     const { jobs } = codec.decode<CodaJobList>(codaJobListSchema, jobsBuffer);
