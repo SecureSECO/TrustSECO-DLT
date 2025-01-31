dbd82f8ccb29b1bf1c6a901f5dd7b61839d0f70c
Martijn Voordouw <martijnvoordouw@gmail.com>
2025-01-31 14:04:08 +0100

    Now using openpgpjs instead of using pgp through the command line
    The previous implementation was incredibly insecure, allowing remote
    command execution. It was also causing inconsistent behaviour

--- common/gpg-verification.ts
+++ common/gpg-verification.ts
@@ -1,49 +1,62 @@
-import { exec, spawnSync } from "child_process";
-import { promisify } from 'util';
 import { codec, Schema } from 'lisk-sdk';
-import { writeFileSync } from 'fs';
-import { unlink } from 'fs/promises';
+import { readFileSync, writeFileSync, existsSync, mkdirSync, readdirSync } from 'fs';
+import { extname } from 'path';
 import { Signed } from '../modules/signed-schemas';
-const exec$ = promisify(exec);
+import axios from 'axios';
+import * as openpgp from 'openpgp';
+
+let keyDir = "/trustseco/keys";
+
+if (!existsSync("/trustseco")){
+    // If trustseco doesn't exist just use the current directory
+    // Otherwise tests don't pass as it is done outside of docker
+    keyDir = "./keys";
+}
+if (!existsSync(keyDir)){
+    mkdirSync(keyDir);
+}
 
 export class GPG {
-    // all GPG key URLs should be provided by github.com
+    /** all GPG key URLs should be provided by github.com */
     static readonly urlPattern = /^https:\/\/github\.com\/([a-z\d](?:[a-z\d]|-(?=[a-z\d])){0,38})\.gpg$/;
 
     static validateURL = ( url: string ) => this.urlPattern.test(url);
 
-    // import a GPG key from a URL to the local GPG keyring
-    // returns the account UID of the imported key
+    /** import a GPG key from a URL to a local file
+    returns the account UID of the imported key */
     static async import( url: string ) : Promise<string> {
-        this.validateURL( url );
-
-        const { stderr } = await exec$(`curl ${url} | gpg --import`);
-        const accountUid = /key \w*(\w{16})/.exec(stderr)?.[1];
+        const match = url.match(this.urlPattern);
+        if (!match) throw new Error(`Url ${url} is not a GitHub GPG link`);
+        const name = match[1];
+        const { data } = await axios.get(url);
+        const key = await openpgp.readKey({armoredKey: data});
+        const accountUid = key.getKeyID().toHex().toUpperCase();
+        writeFileSync(`${keyDir}/${name}.gpg`, data);
         if (accountUid === undefined) throw new Error(`Unable to find the uid for the GPG key from ${url}`);
-
         return accountUid;
     }
 
-    // verify the signature of a signed object
-    // returns the account UID of the key used to sign the object
-    static verify<T extends object>(asset : Signed<T>, schema : Schema) : string {
+    /** verify the signature of a signed object
+    returns the account UID of the key used to sign the object */
+    static async verify<T extends object>(asset : Signed<T>, schema : Schema) : Promise<string> {
         const encoded = codec.encode(schema, asset.data).toString('hex');
+        const keys = await this.readKeys();
+        const signature = await openpgp.readSignature({armoredSignature: asset.signature});
+        const verificationResult = await openpgp.verify({
+            message: await openpgp.createMessage({ text: encoded }),
+            signature,
+            verificationKeys: keys
+        })
+        const { verified, keyID } = verificationResult.signatures[0];
 
-        const random = Math.random().toString().slice(2);
-        writeFileSync("/tmp/signature-" + random, asset.signature);
-        writeFileSync("/tmp/data-" + random, encoded);
-        const { stderr, status } = spawnSync(`gpg`, ["--verify", "/tmp/signature-" + random, "/tmp/data-" + random]);
-        unlink("/tmp/signature-" + random);
-        unlink("/tmp/data-" + random);
-
-        const accountUid = /key \w*(\w{16})/.exec(stderr.toString())?.[1];
-
-        if (status !== 0) {
-            console.log(stderr.toString());
+        try {
+            await verified;
+        }
+        catch {
             if (process.env.ACCEPT_BAD_SIGNATURES) console.error("GPG signature verification failed! ACCEPT_BAD_SIGNATURES is set, so continuing anyway.");
             else throw new Error("GPG signature verification failed");
         }
-
+        const accountUid = keyID.toHex().toUpperCase();
         if (accountUid === undefined) {
             if (process.env.ACCEPT_INVALID_ACCOUNT) {
                 console.error("Unable to find the uid for the provided GPG key! ACCEPT_INVALID_ACCOUNT is set, so continuing anyway with throwaway account");
@@ -51,7 +64,23 @@ export class GPG {
             }
             else throw new Error("Unable to find the uid for the provided GPG key");
         }
-
         return accountUid;
+    }
+
+    /** Read all the pgp files from the keyDir directory and parse them */
+    private static async readKeys() : Promise<openpgp.Key[]> {
+        const keys = [];
+        const files = readdirSync(keyDir);
+        for (const file of files) {
+            if (extname(file) !== ".gpg") continue;
+            try {
+                const data = readFileSync(`${keyDir}/${file}`);
+                keys.push(await openpgp.readKey({armoredKey: '' + data})) // akward implicit type conversion from buffer to string
+            }
+            catch {
+                console.log(`Failed to import key ${file}`)
+            }
+        }
+        return keys;
     }
 }
